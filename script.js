1cd792ddaa66b82c0b074d99a7b345b15f63d46b
qbzaktybijczyk <166698132+qbzaktybijczyk@users.noreply.github.com>
2024-04-11 10:12:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function calculate() {
+    var inputVariable = document.getElementById('inputVariable').value;
+    var result = inputVariable * 2; // Możesz tutaj wprowadzić dowolne obliczenia
+    document.getElementById('result').innerText = "Wynik: " + result;
+}
